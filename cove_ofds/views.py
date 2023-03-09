60aa6aab95cde313eba70ec75d3fd2f237206666
James B <james.baster@opendataservices.coop>
2023-03-09 11:21:19 +0000

    Add URL Upload

--- cove_ofds/views.py
+++ cove_ofds/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from cove_ofds.forms import NewGeoJSONUploadForm
+from cove_ofds.forms import NewGeoJSONUploadForm, NewGeoJSONURLForm
 from cove_ofds.process import (
     AdditionalFieldsChecksTask,
     ConvertCSVsIntoJSON,
@@ -14,6 +14,7 @@ from cove_ofds.process import (
     ConvertJSONIntoGeoJSON,
     ConvertJSONIntoSpreadsheets,
     ConvertSpreadsheetIntoJSON,
+    DownloadDataTask,
     JsonSchemaValidateTask,
     PythonValidateTask,
     WasJSONUploaded,
@@ -38,6 +39,7 @@ class DecimalEncoder(json.JSONEncoder):
 
 GEOJSON_FORM_CLASSES = {
     "upload_form": NewGeoJSONUploadForm,
+    "url_form": NewGeoJSONURLForm,
 }
 
 
@@ -68,30 +70,42 @@ def index(request):
 def new_geojson(request):
 
     forms = {
-        "upload_form": NewGeoJSONUploadForm(request.POST, request.FILES)
-        if request.POST
-        else NewGeoJSONUploadForm()
+        form_name: form_class()
+        for form_name, form_class in GEOJSON_FORM_CLASSES.items()
     }
-    form = forms["upload_form"]
+    request_data = None
+    if request.POST:
+        request_data = request.POST
+    if request_data and (
+        "nodes_file_url" in request_data or "spans_file_url" in request_data
+    ):
+        form_name = "url_form"
+    else:
+        form_name = "upload_form"
+    forms[form_name] = GEOJSON_FORM_CLASSES[form_name](request_data, request.FILES)
+    form = forms[form_name]
     if form.is_valid():
-        # Extra Validation
-        for field in ["nodes_file_upload", "spans_file_upload"]:
-            if field in request.FILES:
-                if (
-                    not request.FILES[field].content_type
-                    in settings.ALLOWED_GEOJSON_CONTENT_TYPES
-                ):
-                    form.add_error(field, "This does not appear to be a JSON file")
-                if not [
-                    e
-                    for e in settings.ALLOWED_GEOJSON_EXTENSIONS
-                    if str(request.FILES[field].name).lower().endswith(e)
-                ]:
-                    form.add_error(field, "This does not appear to be a JSON file")
-        if not ("nodes_file_upload" in request.FILES) and not (
-            "spans_file_upload" in request.FILES
-        ):
-            form.add_error("nodes_file_upload", "You must upload nodes or spans")
+        # Extra Validation For Upload
+        if form_name == "upload_form":
+            for field in ["nodes_file_upload", "spans_file_upload"]:
+                if field in request.FILES:
+                    if (
+                        not request.FILES[field].content_type
+                        in settings.ALLOWED_GEOJSON_CONTENT_TYPES
+                    ):
+                        form.add_error(field, "This does not appear to be a JSON file")
+                    if not [
+                        e
+                        for e in settings.ALLOWED_GEOJSON_EXTENSIONS
+                        if str(request.FILES[field].name).lower().endswith(e)
+                    ]:
+                        form.add_error(field, "This does not appear to be a JSON file")
+            if not ("nodes_file_upload" in request.FILES) and not (
+                "spans_file_upload" in request.FILES
+            ):
+                form.add_error("nodes_file_upload", "You must upload nodes or spans")
+        # Extra Validation For URL
+        # Should check at least one URL passed, but I think that's essentially done by the form_name selector above
 
         # Process
         if form.is_valid():
@@ -99,17 +113,30 @@ def new_geojson(request):
             supplied_data.format = "geojson"
             supplied_data.save()
 
-            if "nodes_file_upload" in request.FILES:
-                supplied_data.save_file(
-                    request.FILES["nodes_file_upload"], meta={"geojson": "nodes"}
-                )
-            if "spans_file_upload" in request.FILES:
-                supplied_data.save_file(
-                    request.FILES["spans_file_upload"], meta={"geojson": "spans"}
-                )
+            if form_name == "upload_form":
+                if "nodes_file_upload" in request.FILES:
+                    supplied_data.save_file(
+                        request.FILES["nodes_file_upload"], meta={"geojson": "nodes"}
+                    )
+                if "spans_file_upload" in request.FILES:
+                    supplied_data.save_file(
+                        request.FILES["spans_file_upload"], meta={"geojson": "spans"}
+                    )
+            if form_name == "url_form":
+                if "nodes_file_url" in form.cleaned_data:
+                    supplied_data.save_file_from_source_url(
+                        form.cleaned_data["nodes_file_url"],
+                        meta={"geojson": "nodes"},
+                        content_type="application/json",
+                    )
+                if "spans_file_url" in form.cleaned_data:
+                    supplied_data.save_file_from_source_url(
+                        form.cleaned_data["spans_file_url"],
+                        meta={"geojson": "spans"},
+                        content_type="application/json",
+                    )
 
             return HttpResponseRedirect(supplied_data.get_absolute_url())
-
     return render(request, "cove_ofds/new_geojson.html", {"forms": forms})
 
 
@@ -119,6 +146,8 @@ def explore_ofds(request, pk):
         return error
 
     PROCESS_TASKS = [
+        # Get data if not already on disk
+        DownloadDataTask(db_data),
         # Make sure uploads are in primary format
         WasJSONUploaded(db_data),
         ConvertSpreadsheetIntoJSON(db_data),
