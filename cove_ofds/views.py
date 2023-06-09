Open-Telecoms-Data/cove-ofds

commit 382f85dc0a07c73d2713f23993ca3bb5d866ed4d

libcoveweb2: Create InputDataView to reduce code duplication
Move input views into cove_ofds.
Other coves will want other input views, so for now keep in app not library.

cove_ofds/views.py
@@ -6,15 +6,18 @@ from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from cove_ofds.forms import NewGeoJSONUploadForm, NewGeoJSONURLForm
+from cove_ofds.forms import (
+    NewCSVsUploadForm,
+    NewGeoJSONUploadForm,
+    NewGeoJSONURLForm,
+    NewJSONTextForm,
+    NewJSONUploadForm,
+    NewJSONURLForm,
+    NewSpreadsheetUploadForm,
+)
 from libcoveweb2.background_worker import process_supplied_data
 from libcoveweb2.models import SuppliedData, SuppliedDataFile
-from libcoveweb2.views import (
-    CSVS_FORM_CLASSES,
-    JSON_FORM_CLASSES,
-    SPREADSHEET_FORM_CLASSES,
-    ExploreDataView,
-)
+from libcoveweb2.views import ExploreDataView, InputDataView
 
 logger = logging.getLogger(__name__)
 
@@ -54,6 +57,98 @@ def index(request):
     }
 
     return render(request, "cove_ofds/index.html", {"forms": forms})
+
+
+JSON_FORM_CLASSES = {
+    "upload_form": NewJSONUploadForm,
+    "text_form": NewJSONTextForm,
+    "url_form": NewJSONURLForm,
+}
+
+
+class NewJSONInput(InputDataView):
+    form_classes = JSON_FORM_CLASSES
+    input_template = "cove_ofds/new_json.html"
+    allowed_content_types = settings.ALLOWED_JSON_CONTENT_TYPES
+    content_type_incorrect_message = "This does not appear to be a JSON file."
+    allowed_file_extensions = settings.ALLOWED_JSON_EXTENSIONS
+    file_extension_incorrect_message = "This does not appear to be a JSON file."
+    supplied_data_format = "json"
+
+    def get_active_form_key(self, forms, request_data):
+        if "paste" in request_data:
+            return "text_form"
+        elif "url" in request_data:
+            return "url_form"
+        else:
+            return "upload_form"
+
+    def save_file_content_to_supplied_data(
+        self, form_name, form, request, supplied_data
+    ):
+        if form_name == "upload_form":
+            supplied_data.save_file(request.FILES["file_upload"])
+        elif form_name == "text_form":
+            supplied_data.save_file_contents(
+                "input.json",
+                form.cleaned_data["paste"],
+                "application/json",
+                None,
+            )
+        elif form_name == "url_form":
+            supplied_data.save_file_from_source_url(
+                form.cleaned_data["url"], content_type="application/json"
+            )
+
+
+CSVS_FORM_CLASSES = {
+    "upload_form": NewCSVsUploadForm,
+}
+
+
+class NewCSVInput(InputDataView):
+    form_classes = CSVS_FORM_CLASSES
+    input_template = "cove_ofds/new_csvs.html"
+    allowed_content_types = settings.ALLOWED_CSV_CONTENT_TYPES
+    content_type_incorrect_message = "This does not appear to be a CSV file."
+    allowed_file_extensions = settings.ALLOWED_CSV_EXTENSIONS
+    file_extension_incorrect_message = "This does not appear to be a CSV file."
+    supplied_data_format = "csvs"
+
+    def get_active_form_key(self, forms, request_data):
+        return "upload_form"
+
+    def save_file_content_to_supplied_data(
+        self, form_name, form, request, supplied_data
+    ):
+        if form_name == "upload_form":
+            for field in form.file_field_names:
+                if request.FILES.get(field):
+                    supplied_data.save_file(request.FILES[field])
+
+
+SPREADSHEET_FORM_CLASSES = {
+    "upload_form": NewSpreadsheetUploadForm,
+}
+
+
+class NewSpreadsheetInput(InputDataView):
+    form_classes = SPREADSHEET_FORM_CLASSES
+    input_template = "cove_ofds/new_spreadsheet.html"
+    allowed_content_types = settings.ALLOWED_SPREADSHEET_CONTENT_TYPES
+    content_type_incorrect_message = "This does not appear to be a spreadsheet."
+    allowed_file_extensions = settings.ALLOWED_SPREADSHEET_EXTENSIONS
+    file_extension_incorrect_message = "This does not appear to be a spreadsheet."
+    supplied_data_format = "csvs"
+
+    def get_active_form_key(self, forms, request_data):
+        return "upload_form"
+
+    def save_file_content_to_supplied_data(
+        self, form_name, form, request, supplied_data
+    ):
+        if form_name == "upload_form":
+            supplied_data.save_file(request.FILES["file_upload"])
 
 
 def new_geojson(request):

libcoveweb2/views.py
@@ -8,171 +8,81 @@ from django.views import View
 
 from libcoveweb2.background_worker import process_supplied_data
 from libcoveweb2.celery import CeleryInspector
-from libcoveweb2.forms import (
-    NewCSVsUploadForm,
-    NewJSONTextForm,
-    NewJSONUploadForm,
-    NewJSONURLForm,
-    NewSpreadsheetUploadForm,
-)
 from libcoveweb2.models import SuppliedData
 from libcoveweb2.process.utils import get_tasks
 
-JSON_FORM_CLASSES = {
-    "upload_form": NewJSONUploadForm,
-    "text_form": NewJSONTextForm,
-    "url_form": NewJSONURLForm,
-}
 
+class InputDataView(View):
+    form_classes = None
+    input_template = None
+    allowed_content_types = None
+    content_type_incorrect_message = "This file is not the correct type."
+    allowed_file_extensions = None
+    file_extension_incorrect_message = "This file is not the correct type."
+    supplied_data_format = None
 
-def new_json(request):
+    def get_active_form_key(self, forms, request_data):
+        return None
 
-    forms = {
-        form_name: form_class() for form_name, form_class in JSON_FORM_CLASSES.items()
-    }
-    request_data = None
-    if request.POST:
-        request_data = request.POST
-    if request_data:
-        if "paste" in request_data:
-            form_name = "text_form"
-        elif "url" in request_data:
-            form_name = "url_form"
-        else:
-            form_name = "upload_form"
-        forms[form_name] = JSON_FORM_CLASSES[form_name](request_data, request.FILES)
-        form = forms[form_name]
-        if form.is_valid():
-            # Extra Validation
-            if form_name == "upload_form":
-                if (
-                    not request.FILES["file_upload"].content_type
-                    in settings.ALLOWED_JSON_CONTENT_TYPES
-                ):
-                    form.add_error(
-                        "file_upload", "This does not appear to be a JSON file"
+    def get_file_field_names_in_form(self, form):
+        return form.file_field_names
+
+    def save_file_content_to_supplied_data(
+        self, form_name, form, request, supplied_data
+    ):
+        pass
+
+    def get(self, request):
+        forms = {
+            form_name: form_class()
+            for form_name, form_class in self.form_classes.items()
+        }
+        return render(request, self.input_template, {"forms": forms})
+
+    def post(self, request):
+        forms = {
+            form_name: form_class()
+            for form_name, form_class in self.form_classes.items()
+        }
+        request_data = None
+        if request.POST:
+            request_data = request.POST
+        if request_data:
+            form_name = self.get_active_form_key(forms, request_data)
+            if form_name:
+                forms[form_name] = self.form_classes[form_name](
+                    request_data, request.FILES
+                )
+                form = forms[form_name]
+                # Extra validation
+                self.validate_content_type_and_file_extensions(form_name, form, request)
+
+                # If fine, save
+                if form.is_valid():
+                    supplied_data = SuppliedData()
+                    supplied_data.format = self.supplied_data_format
+                    supplied_data.save()
+                    self.save_file_content_to_supplied_data(
+                        form_name, form, request, supplied_data
                     )
-                if not [
-                    e
-                    for e in settings.ALLOWED_JSON_EXTENSIONS
-                    if str(request.FILES["file_upload"].name).lower().endswith(e)
-                ]:
-                    form.add_error(
-                        "file_upload", "This does not appear to be a JSON file"
-                    )
-            elif form_name == "text_form":
-                pass  # TODO
+                    process_supplied_data(supplied_data.id)
+                    return HttpResponseRedirect(supplied_data.get_absolute_url())
 
-            # Process
-            if form.is_valid():
-                supplied_data = SuppliedData()
-                supplied_data.format = "json"
-                supplied_data.save()
+        return render(request, self.input_template, {"forms": forms})
 
-                if form_name == "upload_form":
-                    supplied_data.save_file(request.FILES["file_upload"])
-                elif form_name == "text_form":
-                    supplied_data.save_file_contents(
-                        "input.json",
-                        form.cleaned_data["paste"],
-                        "application/json",
-                        None,
-                    )
-                elif form_name == "url_form":
-                    supplied_data.save_file_from_source_url(
-                        form.cleaned_data["url"], content_type="application/json"
-                    )
-
-                process_supplied_data(supplied_data.id)
-                return HttpResponseRedirect(supplied_data.get_absolute_url())
-
-    return render(request, "libcoveweb2/new_json.html", {"forms": forms})
-
-
-CSVS_FORM_CLASSES = {
-    "upload_form": NewCSVsUploadForm,
-}
-
-
-def new_csvs(request):
-
-    forms = {
-        "upload_form": NewCSVsUploadForm(request.POST, request.FILES)
-        if request.POST
-        else NewCSVsUploadForm()
-    }
-    form = forms["upload_form"]
-    if form.is_valid():
-        # Extra Validation
-        for field in form.file_field_names:
+    def validate_content_type_and_file_extensions(self, form_name, form, request):
+        for field in self.get_file_field_names_in_form(form):
             if request.FILES.get(field):
-                if (
-                    not request.FILES[field].content_type
-                    in settings.ALLOWED_CSV_CONTENT_TYPES
+                if self.allowed_content_types and (
+                    not request.FILES[field].content_type in self.allowed_content_types
                 ):
-                    form.add_error(field, "This does not appear to be a CSV file")
-                if not [
+                    form.add_error(field, self.content_type_incorrect_message)
+                if self.allowed_file_extensions and not [
                     e
-                    for e in settings.ALLOWED_CSV_EXTENSIONS
+                    for e in self.allowed_file_extensions
                     if str(request.FILES[field].name).lower().endswith(e)
                 ]:
-                    form.add_error(field, "This does not appear to be a CSV file")
-
-        # Process
-        if form.is_valid():
-            supplied_data = SuppliedData()
-            supplied_data.format = "csvs"
-            supplied_data.save()
-
-            for field in form.file_field_names:
-                if request.FILES.get(field):
-                    supplied_data.save_file(request.FILES[field])
-
-            process_supplied_data(supplied_data.id)
-            return HttpResponseRedirect(supplied_data.get_absolute_url())
-
-    return render(request, "libcoveweb2/new_csvs.html", {"forms": forms})
-
-
-SPREADSHEET_FORM_CLASSES = {
-    "upload_form": NewSpreadsheetUploadForm,
-}
-
-
-def new_spreadsheet(request):
-
-    forms = {
-        "upload_form": NewSpreadsheetUploadForm(request.POST, request.FILES)
-        if request.POST
-        else NewSpreadsheetUploadForm()
-    }
-    form = forms["upload_form"]
-    if form.is_valid():
-        # Extra Validation
-        if (
-            not request.FILES["file_upload"].content_type
-            in settings.ALLOWED_SPREADSHEET_CONTENT_TYPES
-        ):
-            form.add_error("file_upload", "This does not appear to be a spreadsheet")
-        if not [
-            e
-            for e in settings.ALLOWED_SPREADSHEET_EXTENSIONS
-            if str(request.FILES["file_upload"].name).lower().endswith(e)
-        ]:
-            form.add_error("file_upload", "This does not appear to be a spreadsheet")
-
-        # Process
-        if form.is_valid():
-            supplied_data = SuppliedData()
-            supplied_data.format = "spreadsheet"
-            supplied_data.save()
-
-            supplied_data.save_file(request.FILES["file_upload"])
-
-            process_supplied_data(supplied_data.id)
-            return HttpResponseRedirect(supplied_data.get_absolute_url())
-
-    return render(request, "libcoveweb2/new_spreadsheet.html", {"forms": forms})
+                    form.add_error(field, self.file_extension_incorrect_message)
 
 
 class ExploreDataView(View):

cove_ofds/forms.py
@@ -2,6 +2,161 @@ from django import forms
 from django.conf import settings
 
 
+class NewJSONUploadForm(forms.Form):
+    file_field_names = ["file_upload"]
+    file_upload = forms.FileField(
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_JSON_CONTENT_TYPES
+                    + settings.ALLOWED_JSON_EXTENSIONS
+                )
+            }
+        ),
+        label="",
+    )
+
+
+class NewJSONTextForm(forms.Form):
+    file_field_names = []
+    paste = forms.CharField(label="Paste (JSON only)", widget=forms.Textarea)
+
+
+class NewJSONURLForm(forms.Form):
+    file_field_names = []
+    url = forms.URLField(label="URL")
+
+
+class NewSpreadsheetUploadForm(forms.Form):
+    file_field_names = ["file_upload"]
+    file_upload = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_SPREADSHEET_CONTENT_TYPES
+                    + settings.ALLOWED_SPREADSHEET_EXTENSIONS
+                )
+            }
+        ),
+    )
+
+
+class NewCSVsUploadForm(forms.Form):
+    # I know it's hacky to copy and paste code like this but as this needs to be replaced by
+    # something that allows any number of uploads with no limits this will do for now
+    file_field_names = ["file_upload" + str(i) for i in range(0, 10)]
+    file_upload0 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+    )
+    file_upload1 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload2 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload3 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload4 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload5 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload6 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload7 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload8 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+    file_upload9 = forms.FileField(
+        label="",
+        widget=forms.FileInput(
+            attrs={
+                "accept": ",".join(
+                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
+                )
+            }
+        ),
+        required=False,
+    )
+
+
 class NewGeoJSONUploadForm(forms.Form):
     nodes_file_upload = forms.FileField(
         label="Nodes file",

cove_project/urls.py
@@ -10,8 +10,15 @@ handler500 = "libcoveweb2.views.handler500"
 
 urlpatterns += [
     re_path(r"^$", cove_ofds.views.index, name="index"),
-    url(r"^data/(.+)$", cove_ofds.views.ExploreOFDSView.as_view(), name="explore"),
+    re_path(r"^new_json$", cove_ofds.views.NewJSONInput.as_view(), name="new_json"),
+    re_path(r"^new_csvs$", cove_ofds.views.NewCSVInput.as_view(), name="new_csvs"),
+    re_path(
+        r"^new_spreadsheet$",
+        cove_ofds.views.NewSpreadsheetInput.as_view(),
+        name="new_spreadsheet",
+    ),
     re_path(r"^new_geojson$", cove_ofds.views.new_geojson, name="new_geojson"),
+    url(r"^data/(.+)$", cove_ofds.views.ExploreOFDSView.as_view(), name="explore"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

libcoveweb2/forms.py
@@ -1,153 +1,0 @@
-from django import forms
-from django.conf import settings
-
-
-class NewJSONUploadForm(forms.Form):
-    file_upload = forms.FileField(
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_JSON_CONTENT_TYPES
-                    + settings.ALLOWED_JSON_EXTENSIONS
-                )
-            }
-        ),
-        label="",
-    )
-
-
-class NewJSONTextForm(forms.Form):
-    paste = forms.CharField(label="Paste (JSON only)", widget=forms.Textarea)
-
-
-class NewJSONURLForm(forms.Form):
-    url = forms.URLField(label="URL")
-
-
-class NewSpreadsheetUploadForm(forms.Form):
-    file_upload = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_SPREADSHEET_CONTENT_TYPES
-                    + settings.ALLOWED_SPREADSHEET_EXTENSIONS
-                )
-            }
-        ),
-    )
-
-
-class NewCSVsUploadForm(forms.Form):
-    # I know it's hacky to copy and paste code like this but as this needs to be replaced by
-    # something that allows any number of uploads with no limits this will do for now
-    file_field_names = ["file_upload" + str(i) for i in range(0, 10)]
-    file_upload0 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-    )
-    file_upload1 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload2 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload3 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload4 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload5 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload6 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload7 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload8 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )
-    file_upload9 = forms.FileField(
-        label="",
-        widget=forms.FileInput(
-            attrs={
-                "accept": ",".join(
-                    settings.ALLOWED_CSV_CONTENT_TYPES + settings.ALLOWED_CSV_EXTENSIONS
-                )
-            }
-        ),
-        required=False,
-    )

commit 35082948dcbc2dbdda1bf2086b4e396df1153471

bugfix: Uploading spreadsheet was broken
Broken in 382f85dc

https://github.com/Open-Telecoms-Data/cove-ofds/issues/95

cove_ofds/views.py
@@ -139,7 +139,7 @@ class NewSpreadsheetInput(InputDataView):
     content_type_incorrect_message = "This does not appear to be a spreadsheet."
     allowed_file_extensions = settings.ALLOWED_SPREADSHEET_EXTENSIONS
     file_extension_incorrect_message = "This does not appear to be a spreadsheet."
-    supplied_data_format = "csvs"
+    supplied_data_format = "spreadsheet"
 
     def get_active_form_key(self, forms, request_data):
         return "upload_form"